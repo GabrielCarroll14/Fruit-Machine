945e1a0d17e02c57b8def3b654a4c1514d55f49b
Gabriel Carroll <142114114+gabriel8877@users.noreply.github.com>
2023-12-24 16:11:01 +0000

    Initial Commit

---
+++ slotmachine.py
@@ -0,0 +1,83 @@
+# Create a random password for money import called randompass1
+randompass1 = "1234"
+
+# Import the random module
+import random
+
+# create the user's balance
+Balance = 100
+
+# Startup the machine and give the user an introductory message
+print("Welcome to Slot-Machine, increase your balance to earn more rewards:")
+
+# Give the user the option to start up or import more balance
+startup = input("To play straight away, press P. To import more balance, press I: ")
+
+# The option where the user imports more money
+if startup == "I":
+    print("You have chosen to import more money to your balance.")
+    upass = input("Please input the password to import... ")
+
+    # ask the user how much they would like to import
+    if upass == randompass1:
+        print("You have chosen to import more money to your balance.")
+        uamount = int(input("How much would you like to import? "))
+        Balance = Balance + uamount
+        print("Your balance is now £"  + str(Balance) + ". ")
+    else:
+        print("Incorrect password. Import aborted.")
+else:
+    print("You have chosen to play straight away.")
+
+# Start the game and begin the loop
+for number in range(1000):
+    
+    # As the user how much they want to bet
+    Amount = int(input("How much would you like to bet? "))
+    
+    ## If the amount bet is 0 break the loop
+    if Amount < 1:
+        print ("You cannot bet 0, the game will now reset")
+        break
+   
+    # Subtract the amount the user has bet
+    Balance = Balance - Amount
+    
+    # Randomize the wheels.
+    spin1 = random.choice(["Cherry-🍒", "Orange-🍊", "Plum---🫐", "Bell---🔔", "Bar----🍫", "Seven--❼"])
+    spin2 = random.choice(["Cherry-🍒", "Orange-🍊", "Plum---🫐", "Bell---🔔", "Bar----🍫", "Seven--❼"])
+    spin3 = random.choice(["Cherry-🍒", "Orange-🍊", "Plum---🫐", "Bell---🔔", "Bar----🍫", "Seven--❼"])
+    
+    # User win scenario.
+    if spin1 == spin2 == spin3:
+        Balance = Balance + Amount * 2
+        print (" ")
+        print (spin1)
+        print (spin2)
+        print (spin3)
+        print (" ")
+        print("You won! ")
+        print("Your balance is now £" + str(Balance) + "! ")
+        
+        # Give user a warning if their ballance falls below the recommended amount.
+        if Balance < 15:
+            print ("WARNING! BALANCE EXTREMELY LOW! ")
+    
+    # User lose scenario.    
+    else:
+        print (" ")
+        print (spin1)
+        print (spin2)
+        print (spin3)
+        print (" ")
+        print ("You lost! ")
+        print("Your balance is now £" + str(Balance) + "! ")
+        
+        # Give user a warning if their ballance falls below the recommended amount.
+        if Balance < 15:
+            print ("WARNING! BALANCE EXTREMELY LOW! ")
+    
+    # Restart the game if they go bankrupt.        
+    if Balance < 1:
+        print ("You have gone bankrupt, the game will now reset. ")
+        break
