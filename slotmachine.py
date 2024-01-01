33132043bcfe021c6e46d0b7e16ac0d251f34787
Gabriel Carroll <gabrielcarroll11@gmail.com>
2024-01-01 12:24:52 +0000

    Update slotmachine.py

--- slotmachine.py
+++ slotmachine.py
@@ -7,48 +7,73 @@ score = 0
 # Import the random module
 import random
 
-# Spin wheels function
-def spin_wheels():
-    spin1 = random.choice(["Cherry 🍒", "Orange 🍊", "Plum   🫐", "Bell   🔔", "Bar    🍫", "Seven  ❼"])
-    spin2 = random.choice(["Cherry 🍒", "Orange 🍊", "Plum   🫐", "Bell   🔔", "Bar    🍫", "Seven  ❼"])
-    spin3 = random.choice(["Cherry 🍒", "Orange 🍊", "Plum   🫐", "Bell   🔔", "Bar    🍫", "Seven  ❼"])
+# Start of game setup
 
-# Login function    
-def login():
-    while True:
+# Run the login function
+while True:
         print ("Welcome to Slot-Machine!")
         u_name = input ("Please enter a username. ")
         print ("Your user name will be " + u_name + ". ")
         choice = input ("Are you sure you would like this to be your user name? (y, n) ")
         if choice == "y":
+            print ("Great! Your name will be " + u_name + "! ")
             break
         if choice == "n":
             print ("You have decided to reset your user name.")
 
-# Enter bet function           
-def bet():
-    amount = int(input("How much would you like to bet? "))
-    balance = balance - amount
-
-# Function to justify user win
-def user_win():
-    if spin1 == spin2 == spin3:
-        balance = balance + amount * 2
-        print (" ")
-        print (spin1)
-        print (spin2)
-        print (spin3)
-        print (" ")
-        print ("You win your balance is now £" + balance + "! ")
-
-def user_lose():
+while True:
+    print ("What would you like to do? View high scores or play? (play, view) ")
+    vorp = input ("Please select an option. ")
     
+    if vorp == "play":
+        amount = int(input("How much would you like to bet? "))
+        balance = balance - amount
 
-# Update score function   
-def update_score():
-    if balance > score:
-        with open ("scores.txt", "a") as scores:
-            scores.write (" " + u_name + " | " + str(score) + "\n")
+        spin1 = random.choice(["Cherry 🍒", "Orange 🍊", "Plum   🫐", "Bell   🔔", "Bar    🍫", "Seven  ❼"])
+        spin2 = random.choice(["Cherry 🍒", "Orange 🍊", "Plum   🫐", "Bell   🔔", "Bar    🍫", "Seven  ❼"])
+        spin3 = random.choice(["Cherry 🍒", "Orange 🍊", "Plum   🫐", "Bell   🔔", "Bar    🍫", "Seven  ❼"])
+
+        # User win scenario.
+        if spin1 == spin2 == spin3:
+            balance = balance + amount * 2
+            print (" ")
+            print (spin1)
+            print (spin2)
+            print (spin3)
+            print (" ")
+            print("You won! ")
+            print ("Your balance is now £" + str(balance) + "! ")
+            if balance > score:
+                print ("New high score of " + score + "! ")
+                score = balance
+                with open ("scores.txt", "a") as scores:
+                    scores.write (" " + u_name + " | £" + str(score) + "\n")
+                
+        # User lose scenario.    
+        else:
+            print (" ")
+            print (spin1)
+            print (spin2)
+            print (spin3)
+            print (" ")
+            print ("You lost! ")
+            print("Your balance is now £" + str(balance) + "! ")
+            if balance > score:
+                print ("New high score of " + score + "! ")
+                score = balance
+                with open ("scores.txt", "a") as scores:
+                    scores.write (" " + u_name + " | £" + str(score) + "\n")
+                    
+    elif vorp == "view":
+        with open("scores.txt", "r") as scores:
+            content = scores.read()
+            print(content)
+
+        
+        
+        
+
+
             
 
             
